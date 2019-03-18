2d37bd057e7e7b8649255cf4108e5570dc74a743
Emory Griffith <emorygriffith91@gmail.com>
2019-03-18 16:24:58 -0400

    added gif to 404 page

--- 404.php
+++ 404.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?>
 
-			<div id="content">
-				<h1>404 - No Page here :(</h1>
-			</div>
+	<div id="content">
+		<h1>404 :(</h1>
+		<p>Unfortunately the page you're trying to visit isn't here. Maybe you typed in 
+			an address that isn't valid? (fingers crossed) Or maybe I messed something up on 
+			my end. Sorry about that.
+		</p>
+
+		<iframe style="padding-top: 20px; padding-bottom: 20px;" src="https://giphy.com/embed/jVFgWDtkY5v2M" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+	</div>
 
 <?php get_footer(); ?>
